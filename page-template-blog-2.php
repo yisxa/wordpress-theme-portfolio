921646affdd251968ee2a692189635c2ad8ea2d7
Cyberabc <yisxaa@gmail.com>
2019-10-21 05:34:34 +0530

    blog section added

---
+++ page-template-blog-2.php
@@ -0,0 +1,68 @@
+<?php
+/*
+Template Name: Blog Page Template 2
+*/
+?>
+
+
+	<?php get_header(); ?>
+<br>
+<br>
+<br>
+	<section class="blog py-5 bg-dark">
+
+		<div class="container">
+			<div class="row">
+				<div class="col-12 ">
+					<h3 class="heading-6  text-center text-white">Latest Blogs</h3>
+					<hr class="mx-auto">
+				</div>
+				<div class="col-sm-3">
+					<div class="card">
+						<img src="http://7englishielts.com/img/projects/5.jpg" class="w-100">
+
+						<div class="card-block">
+							<div class="category">Blog</div>
+							<a href="#"><h3 class="card-title">Upcoming  Facebook Ad Format Update. Hear it first from us</h3></a>
+						</div>
+					</div>
+
+
+				</div>
+
+
+				<div class="col-sm-3">
+					<div class="card">
+						<img src="http://7englishielts.com/img/projects/2.jpg" class="w-100">
+						<!--  <div class="card-img-top" alt="Card image cap"></div>-->
+						<div class="card-block">
+							<div class="category">Blog</div>
+							<a href="#"><h3 class="card-title">Organic and Paid advertisements. Which is better ?</h3></a>
+						</div>
+					</div>
+
+
+				</div>
+
+
+				<div class="col-sm-3">
+					<div class="card">
+						<img src="http://accudata.co.in/blog3.jpg" class="w-100">
+						<!--  <div class="card-img-top" alt="Card image cap"></div>-->
+						<div class="card-block">
+							<div class="category">Blog</div>
+							<a href="#"><h3 class="card-title">Top 3 UI/UX Website Design Trends in 2019</h3></a>
+						</div>
+					</div>
+
+
+				</div>
+
+			</div>
+		</div>
+	</section>
+
+	<?php get_footer(); ?>
+
+
+
--- single.php
+++ single.php
@@ -1,22 +1,33 @@
 	<!-- for getting the header.php link to the index.php -->
 	<?php get_header(); ?>
-	<br><br><br>
+	<hr style="color:blue; width:2px;">
+
+	<div class="section" id="b-section-header" name="Header">
+		<div class="widget Header" data-version="2" id="Header1">
+			<div class="header image-placement-behind no-image">
+				<div class="container">
+					<h1><a href="">JavaScript Tutorial</a></h1>
+					<p>JavaScript</p>
+				</div>
+			</div>
+		</div>
+	</div>
 	<div class="main">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-8">
+			<div class="col-lg-8">
 				<?php if(have_posts()) : ?>
 					<?php while(have_posts()) : the_post(); ?>
 						<!-- post content from template parts content-single -->
 						<?php get_template_part('template-parts/content', 'single'); ?>
 						<br>
 						<!-- Add comments to the single post -->
-						<?php 
-							if ( comments_open() || get_comments_number() ){
-								comments_template();
-							}
+						<?php
+						if ( comments_open() || get_comments_number() ){
+							comments_template();
+						}
 
-						 ?>
+						?>
 					<?php endwhile; ?>
 					<?php else : ?>
 						<?php echo wpautop("No posts were found"); ?>
@@ -24,14 +35,48 @@
 				</div>
 
 				<div class="col-md-4">
-					<br>
-					<!-- for getting the sidebar.php link to the index.php-->
+					<!-- Search Widget -->
+			          <div class="card my-4">
+			            <h5 class="card-header">Search</h5>
+			            <div class="card-body">
+			              <div class="input-group">
+			                <input type="text" class="form-control" placeholder="Search for...">
+			                <span class="input-group-btn">
+			                  <button class="btn btn-secondary" type="button">Go!</button>
+			                </span>
+			              </div>
+			            </div>
+			          </div>
 					<?php get_sidebar(); ?>
+					         <!-- Side Widget -->
+					         <div class="card my-4">
+					           <h5 class="card-header">maelezo</h5>
+					           <div class="card-body">
+					             You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
+					           </div>
+					         </div>
+					<!-- Side Widget -->
+						<div class="card my-4">
+							<h5 class="card-header">banner</h5>
+							<div class="card-body">
+								<p>You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!</p>
+							</div>
+						</div>
+			         <div class="card my-4">
+			         	<h5 class="card-header">Tweeter here</h5>
+			         	<div class="card-body">
+
+			         		<a class="twitter-timeline" href="https://twitter.com/apple?">Tweets by Bandhu</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+			         		<a href="https://twitter.com/sample_cool?" class="twitter-follow-button" data-show-count="false">Follow @Bandhu</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+			         	</div>
+			         </div>
 				</div>
 			</div>
-			
+
 		</div>
 	</div>
+
+
 
 	<!-- for getting the footer.php link to the index.php-->
 	<?php get_footer(); ?>
